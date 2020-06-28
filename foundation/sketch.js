7ab6da704b99c6b072df0f923d47f327df926133
Edzard Höfig <edzard.hoefig@beuth-hochschule.de>
2020-06-28 21:49:09 +0200

    Foundation initial commit

---
+++ foundation/sketch.js
@@ -0,0 +1,15 @@
+function init() {
+	// One time init	
+}
+
+function update() {
+	// Update calculations
+}
+
+function paint() {
+	// Paint a frame
+}
+
+function resize() {
+	// Adapt to new size of canvas
+}
